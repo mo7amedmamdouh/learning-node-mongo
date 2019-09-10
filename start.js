15ca7f1ea0c03fd7555c82c0f1a6df93ee463055
Mohamed Mamdouh <m_mamdouh@outlook.com>
2019-09-11 00:47:48 +0200

    Creating Structure of application , connecting to mongo and add schema

---
+++ start.js
@@ -0,0 +1,22 @@
+require('./models/Registration');
+const app = require('./app');
+require('dotenv').config();
+
+// Connect TO Mongo
+
+const mongoose = require('mongoose');
+mongoose.connect(process.env.DATABASE, { useNewUrlParser: true,  useUnifiedTopology: true } );
+mongoose.Promise = global.Promise;
+mongoose.connection
+  .on('connected', () => {
+    console.log(`Mongoose connection open on ${process.env.DATABASE}`);
+  })
+  .on('error', (err) => {
+    console.log(`Connection error: ${err.message}`);
+  });
+
+
+
+const server = app.listen(3000, () => {
+  console.log(`Express is running on port ${server.address().port}`);
+});
